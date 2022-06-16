16ff316f5c33707e04bfaf4f71d18b81c4103a78
Rodrigo Barba <a20490687@itmexicali.edu.mx>
2022-06-15 22:23:01 -0700

    feat: adding /three folder

---
+++ three/WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Nombre: " + textBox1.Text + "\n"
+                + "Carrera: " + textBox2.Text + "\n"
+                + "Semestre: " + textBox3.Text + "\n"
+                + "Color Favorito: " + textBox4.Text + "\n"
+                + "Comida Favorita: " + textBox5.Text + "\n"
+                );
+        }
+    }
+}
---
+++ three/WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) + Convert.ToDouble(textBox2.Text));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) - Convert.ToDouble(textBox2.Text));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox2.Text));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double result = 0;
+            if (Convert.ToDouble(textBox2.Text) != 0)
+            {
+                result = Convert.ToDouble(textBox1.Text) / Convert.ToDouble(textBox2.Text);
+                textBox3.Text = Convert.ToString(result);
+            }
+            else
+            {
+                textBox3.Text = "Error: Divided by 0";
+            }
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+    }
+}
